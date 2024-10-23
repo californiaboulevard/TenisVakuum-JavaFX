cadef8ab91273bb539ced24af0f8c198ba505df0
californiaboulevard <adam.vojtus6@gmail.com>
2024-10-23 15:16:40 +0200

    minor changes

--- src/main/java/com/vysoka/tenis_vakuum/HelloController.java
+++ src/main/java/com/vysoka/tenis_vakuum/HelloController.java
@@ -5,6 +5,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.fxml.FXML;
 
+import javax.lang.model.type.NullType;
+import java.util.Arrays;
+
 public class HelloController {
     @FXML
     private Label label3;
@@ -12,7 +15,6 @@ public class HelloController {
     private Label label1;
     @FXML
     private Label label2;
-
 
     @FXML
     private TextField textField1 = new TextField();
@@ -29,40 +31,49 @@ public class HelloController {
     @FXML
     private TextField textField7 = new TextField();
     @FXML
-    private TextField textField8 = new TextField();
-    @FXML
     private Button button = new Button();
 
     private static final double g = -9.81;
-    private static int N;
+    private static int N = 500;
 
-    private double dt;
-    private double vx;
-    private double vz;
-    private double m;
+    private double dt = 0.001;
+    private double vx = 35.0;
+    private double vz = 0.0;
+    private double m = 10.0;
     private static double[] xpole;
     private static double[] zpole;
 
-
-    private double x;
-    private double z;
+    private double x = 0.0;
+    private double z = 1.5;
 
     @FXML
     protected void action() {
 
-        m = Double.parseDouble(textField3.getText());
-        N = Integer.parseInt(textField2.getText());
-        x = Double.parseDouble(textField4.getText());
-        z = Double.parseDouble(textField5.getText());
-        z = Double.parseDouble(textField5.getText());
-        dt = Double.parseDouble(textField1.getText());
-        vx = Double.parseDouble(textField6.getText());
-        vz = Double.parseDouble(textField7.getText());
-        xpole = new double[N+1];
-        zpole = new double[N+1];
+        if (!textField3.getText().isEmpty()) {
+            m = Double.parseDouble(textField3.getText());
+        }
+        if (!textField1.getText().isEmpty()) {
+            dt = Double.parseDouble(textField1.getText());
+        }
+        if (!textField2.getText().isEmpty()) {
+            N = Integer.parseInt(textField2.getText());
+        }
+        if (!textField4.getText().isEmpty()) {
+            x = Double.parseDouble(textField4.getText());
+        }
+        if (!textField5.getText().isEmpty()) {
+            z = Double.parseDouble(textField5.getText());
+        }
+        if (!textField6.getText().isEmpty()) {
+            vx = Double.parseDouble(textField6.getText());
+        }
+        if (!textField7.getText().isEmpty()) {
+            vz = Double.parseDouble(textField7.getText());
+        }
+        xpole = new double[N + 1];
+        zpole = new double[N + 1];
         xpole[0] = x;
         zpole[0] = z;
-
 
         for (int i = 1; i <= N; i++) {
 
@@ -73,11 +84,14 @@ public class HelloController {
             vx = vx; // stays constant in this scenario
             vz = vz + g * dt;
 
-
         }
         label1.setText("" + xpole[N]);
         label2.setText("" + zpole[N]);
 
+        x = 0.0;
+        z = 1.5;
+        vx = 35.0;
+        vz = 0.0;
 
         if (z == 0.0) {
             label3.setText("");
@@ -88,5 +102,8 @@ public class HelloController {
         } else if (z > 0.0) {
             label3.setText("");
         }
+
+
     }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.vysoka.tenis_vakuum {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
+    requires java.compiler;
 
     opens com.vysoka.tenis_vakuum to javafx.fxml;
     exports com.vysoka.tenis_vakuum;
